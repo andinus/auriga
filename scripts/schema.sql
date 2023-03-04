32cb8449d644b94b8b102dc77797cc6e321c14c9
Andinus <andinus@nand.sh>
2023-03-04 17:48:47 +0530

    Update schema to add details, events, volunteers

--- scripts/schema.sql
+++ scripts/schema.sql
@@ -2,6 +2,31 @@ CREATE TABLE IF NOT EXISTS account(
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     created DATETIME DEFAULT CURRENT_TIMESTAMP,
     email TEXT NOT NULL UNIQUE,
-    password TEXT NOT NULL,
-    ngo_name TEXT NOT NULL
+    password TEXT NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS ngo_detail(
+    ngo_id INTEGER REFERENCES account(id),
+    name TEXT NOT NULL,
+    description TEXT NOT NULL,
+    theme TEXT NOT NULL CHECK( theme IN ('cover') ),
+    published BOOLEAN DEFAULT FALSE
+);
+
+CREATE TABLE IF NOT EXISTS ngo_event(
+    ngo_id INTEGER REFERENCES account(id),
+    created DATETIME DEFAULT CURRENT_TIMESTAMP,
+    name TEXT NOT NULL,
+    description TEXT NOT NULL,
+    starts DATE NOT NULL,
+    ends DATE NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS ngo_volunteer(
+    ngo_id INTEGER REFERENCES account(id),
+    created DATETIME DEFAULT CURRENT_TIMESTAMP,
+    name TEXT NOT NULL,
+    phone TEXT NOT NULL,
+    email TEXT NOT NULL,
+    introduction TEXT NOT NULL
 );
